fb8b80f8b6e1009c528d80a30693ae83bb18157c
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-05-11 23:24:09 +0530

    Add Scroll view and added button

--- react_native_init/src/app/index.tsx
+++ react_native_init/src/app/index.tsx
@@ -1,72 +1,42 @@
-import { View, Text, Image, TextInput, Pressable } from "react-native";
-import { useState } from "react";
+import { StyleSheet, Text, View, ScrollView, Button } from "react-native";
+import React from "react";
 
-export default function HomeScreen() {
-  const [name, setName] = useState("");
+const HomeScreen = () => {
+  const items = Array.from({ length: 200 }, (_, i) => `Item ${i + 1}`);
   return (
-    <View>
-      <Text numberOfLines={6}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur eius
-        laboriosam, illo aspernatur deleniti magnam quisquam hic delectus
-        maiores maxime accusamus provident ad voluptates tempora totam
-        consectetur! Similique reiciendis atque voluptate ipsum praesentium
-        facilis nam eum? Tempore ex voluptate totam nobis et excepturi a
-        eligendi, magni quod? Quis autem cupiditate modi, quas rerum architecto
-        alias at perferendis molestiae enim itaque?
-      </Text>
-      {/* Three ways to load images */}
-      {/* Remote image from internet */}
-      <Image
-        source={{
-          uri: "https://cdn.dribbble.com/userupload/42554942/file/original-8c08872b10a618de098360d1ef04f0b7.png?resize=845x634&vertical=center",
-        }}
-        width={100}
-        height={100}
+    <ScrollView
+      style={{ flex: 1, backgroundColor: "tomato" }}
+      contentContainerStyle={{
+        padding: 16,
+        alignItems: "center",
+      }}
+    >
+      {items.map((item) => (
+        <View
+          key={item}
+          style={{
+            backgroundColor: "white",
+            padding: 16,
+            borderRadius: 10,
+            marginBottom: 10,
+            shadowColor: "#000", // used for IOS shadow
+            shadowOpacity: 0.05, // used for IOS shadow
+            shadowRadius: 4, // used for IOS shadow
+            elevation: 2, // used for Android shadow
+          }}
+        >
+          <Text style={{ fontSize: 16 }}>{item}</Text>
+        </View>
+      ))}
+      <Button
+        title="Hello i am button"
+        color={"green"}
+        onPress={() => alert("Hello world")}
       />
-      {/* Local image from directory */}
-      <Image
-        source={require("@/assets/images/icon.png")}
-        style={{
-          height: 100,
-          width: 100,
-        }}
-        blurRadius={12}
-      />
-      <TextInput
-        placeholder="enter your name"
-        value={name}
-        onChangeText={setName}
-        placeholderTextColor={"tomato"}
-        style={{
-          borderWidth: 1,
-          borderColor: "#000",
-          margin: 10,
-          fontSize: 16,
-          borderRadius: 6,
-        }}
-      />
-
-      <Pressable
-        // onLongPress={}
-        // onPressIn={}
-        // onPressOut={}
-        onPress={() => alert("Button Pressed")}
-        style={({ pressed }) => ({
-          backgroundColor: pressed ? "#f97474" : "#87fd54",
-          color: "#fff",
-        })}
-        hitSlop={{
-          top: 10,
-          bottom: 10,
-          right: 10,
-          left: 10,
-        }}
-      >
-        {({ pressed }) =>
-          pressed ? <Text>Processing</Text> : <Text>Pressed</Text>
-        }
-        {/* <Text>Press Me</Text> */}
-      </Pressable>
-    </View>
+    </ScrollView>
   );
-}
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
