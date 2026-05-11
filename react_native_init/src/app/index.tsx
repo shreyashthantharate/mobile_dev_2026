26a4649ea425aaa6173945a2b1615169d643b08f
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-05-12 00:32:48 +0530

    Add flatview

--- react_native_init/src/app/index.tsx
+++ react_native_init/src/app/index.tsx
@@ -1,11 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import React from "react";
 
 const HomeScreen = () => {
+  const USERS = [
+    { id: "1", name: "Alice Johnson", role: "Designer" },
+    { id: "2", name: "Bob Smith", role: "Developer" },
+    { id: "3", name: "Carol White", role: "Manager" },
+    { id: "4", name: "David Brown", role: "Developer" },
+    { id: "5", name: "Eve Davis", role: "Designer" },
+  ];
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <FlatList
+      style={{
+        backgroundColor: "red",
+      }}
+      data={USERS}
+      horizontal // make view horizontal
+      keyExtractor={(item) => item.id}
+      contentContainerStyle={{ padding: 16, backgroundColor: "green" }}
+      renderItem={({ item }) => <Text>{item.name}</Text>}
+      ItemSeparatorComponent={() => (
+        <View style={{ height: 1, backgroundColor: "black" }} />
+      )}
+    />
   );
 };
 
