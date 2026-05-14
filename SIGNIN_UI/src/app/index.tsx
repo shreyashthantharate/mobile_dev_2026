0d6758cf57f35a0f68864c860f8de2e88f196157
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-05-14 23:34:50 +0530

    Added basic elements for the basic UI

--- SIGNIN_UI/src/app/index.tsx
+++ SIGNIN_UI/src/app/index.tsx
@@ -1,17 +1,82 @@
-import { Text, View, StyleSheet } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Pressable,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import React, { useState } from "react";
 
-export default function Index() {
+const HomeScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Image
+          source={require("@/assets/images/logo.png")}
+          style={{
+            height: 100,
+            width: 100,
+          }}
+          blurRadius={12}
+        />
+        <Text
+          style={{
+            fontSize: 36,
+            textAlign: "center",
+            fontWeight: "bold",
+          }}
+        >
+          Sign In
+        </Text>
+        <Text
+          style={{
+            textAlign: "center",
+            paddingTop: 8,
+          }}
+        >
+          Let's experience the joy of telecare AI.
+        </Text>
+        <Text
+          style={{
+            fontWeight: "bold",
+          }}
+        >
+          Email Address
+        </Text>
+        <TextInput
+          // <Image source={require()}/>
+          style={{
+            fontSize: 20,
+            // borderWidth: 1,
+            shadowColor: "#000",
+            shadowOpacity: 0.02,
+            shadowRadius: 4,
+            elevation: 1,
+          }}
+          placeholder="Enter your email"
+          value={email}
+          onChangeText={setEmail}
+        />
+        <Text>Password</Text>
+        <TextInput
+          placeholder="Enter your password"
+          value={password}
+          onChangeText={setPassword}
+        />
+        <Pressable onPress={() => alert("Button Pressed")}>
+          {({ pressed }) =>
+            pressed ? <Text>Signing In...</Text> : <Text>Sign In</Text>
+          }
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
